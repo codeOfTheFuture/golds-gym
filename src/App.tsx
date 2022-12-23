3b24133e98dd30eb51eba4f7b5dee1c70803c2b4
Jeff Oliver <jeffoliver3384@gmail.com>
2022-12-23 05:41:09 -0500

    Navbar ui completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Gold Gym</h1>
-    </div>
+    <Box
+      className="App"
+      width="400px"
+      sx={{ width: { xl: "1488px" } }}
+      m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
---
+++ src/pages/ExerciseDetail.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const ExerciseDetail: FC = () => {
+  return (
+    <div>
+      <h2>ExerciseDetail</h2>
+    </div>
+  );
+};
+
+export default ExerciseDetail;
